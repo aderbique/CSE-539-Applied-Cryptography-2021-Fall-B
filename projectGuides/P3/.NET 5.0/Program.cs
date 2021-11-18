afba06f978c75f3f83cee21613389386e5c69670
Austin Derbique <austin@derbique.org>
2021-11-17 21:37:16 -0700

    finish P3

--- projectGuides/P3/.NET 5.0/Program.cs
+++ projectGuides/P3/.NET 5.0/Program.cs
@@ -1,3 +1,8 @@
+/* 
+Author: austin@derbique.org
+This is published to GH for educational purposes only. Academic dishonesty will not be tolerated.
+Permission to use this code for course credit is strictly PROHIBITED.
+*/
 using System;
 using System.Security.Cryptography; // Aes
 using System.Numerics; // BigInteger
@@ -7,43 +12,125 @@ namespace P3
 {
     class Program
     {
+        static byte[] get_bytes_from_string(string input)
+        //Taken from https://gist.github.com/GiveThanksAlways/df9e0fa9e7ea04d51744df6a325f7530
+        {
+            var input_split = input.Split(' ');
+            byte[] inputBytes = new byte[input_split.Length];
+            int i = 0;
+            foreach (string item in input_split)
+            {
+                inputBytes.SetValue(Convert.ToByte(item, 16), i);
+                i++;
+            }
+            return inputBytes;
+        }
+        static byte[] EncryptStringToBytes_Aes(string plainText, byte[] Key, byte[] IV)
+        //Pulled from https://docs.microsoft.com/en-us/dotnet/api/system.security.cryptography.aes?view=net-5.0
+        {
+            // Check arguments.
+            if (plainText == null || plainText.Length <= 0)
+                throw new ArgumentNullException("plainText");
+            if (Key == null || Key.Length <= 0)
+                throw new ArgumentNullException("Key");
+            if (IV == null || IV.Length <= 0)
+                throw new ArgumentNullException("IV");
+            byte[] encrypted;
+
+            // Create an Aes object
+            // with the specified key and IV.
+            using (Aes aesAlg = Aes.Create())
+            {
+                aesAlg.Key = Key;
+                aesAlg.IV = IV;
+
+                // Create an encryptor to perform the stream transform.
+                ICryptoTransform encryptor = aesAlg.CreateEncryptor(aesAlg.Key, aesAlg.IV);
+
+                // Create the streams used for encryption.
+                using (MemoryStream msEncrypt = new MemoryStream())
+                {
+                    using (CryptoStream csEncrypt = new CryptoStream(msEncrypt, encryptor, CryptoStreamMode.Write))
+                    {
+                        using (StreamWriter swEncrypt = new StreamWriter(csEncrypt))
+                        {
+                            //Write all data to the stream.
+                            swEncrypt.Write(plainText);
+                        }
+                        encrypted = msEncrypt.ToArray();
+                    }
+                }
+            }
+
+            // Return the encrypted bytes from the memory stream.
+            return encrypted;
+        }
+
+        static string DecryptStringFromBytes_Aes(byte[] cipherText, byte[] Key, byte[] IV)
+        //Pulled from https://docs.microsoft.com/en-us/dotnet/api/system.security.cryptography.aes?view=net-5.0
+        {
+            // Check arguments.
+            if (cipherText == null || cipherText.Length <= 0)
+                throw new ArgumentNullException("cipherText");
+            if (Key == null || Key.Length <= 0)
+                throw new ArgumentNullException("Key");
+            if (IV == null || IV.Length <= 0)
+                throw new ArgumentNullException("IV");
+
+            // Declare the string used to hold
+            // the decrypted text.
+            string plaintext = null;
+
+            // Create an Aes object
+            // with the specified key and IV.
+            using (Aes aesAlg = Aes.Create())
+            {
+                aesAlg.Key = Key;
+                aesAlg.IV = IV;
+
+                // Create a decryptor to perform the stream transform.
+                ICryptoTransform decryptor = aesAlg.CreateDecryptor(aesAlg.Key, aesAlg.IV);
+
+                // Create the streams used for decryption.
+                using (MemoryStream msDecrypt = new MemoryStream(cipherText))
+                {
+                    using (CryptoStream csDecrypt = new CryptoStream(msDecrypt, decryptor, CryptoStreamMode.Read))
+                    {
+                        using (StreamReader srDecrypt = new StreamReader(csDecrypt))
+                        {
+
+                            // Read the decrypted bytes from the decrypting stream
+                            // and place them in a string.
+                            plaintext = srDecrypt.ReadToEnd();
+                        }
+                    }
+                }
+            }
+
+            return plaintext;
+        }
 
         public static string P3(string[] args)
         {
-            // Make sure you are familiar with the System.Numerics.BigInteger class and how to use some of the functions it has (Parse, Pow, ModPow, Subtract, ToByteArray, etc.)
-            
-            // optional hint: for encryptiong/ decryption with AES, use google or another search engine to find the microsoft documentation on Aes (google this--> System.Security.Cryptography.Aes)
-            // you can copy and paste the example code from microsoft documentation if you want as well. https://docs.microsoft.com/en-us/dotnet/api/system.security.cryptography.aes?view=net-5.0
-
-            // optional hint: here is an example of how to convert the IV input string to a byte array https://gist.github.com/GiveThanksAlways/df9e0fa9e7ea04d51744df6a325f7530
-
-            // you will be using BigInteger functions for almost all, if not all mathmatical operations. (Pow, ModPow, Subtract)
-            // N = 2^(N_e) - N_c (this calculation needs to be done using BigInteger.Pow and BigInteger.Subtract)
-
-            // Diffie-Hellman key is g^(xy) mod N. In the input you are given g_y which is g^y. So to make the key you need to perform g_y^(x) using the BigInteger class
-            // key = g_y^(x) mod N (this calculation needs to be done using BigInteger.ModPow)
-
-            // you can convert a BigInteger into a byte array using the BigInteger.ToByteArray() function/method
-
-            Console.WriteLine("just printing the input");
-            foreach(var item in args)
-            {
-                Console.WriteLine(item); //DELETE, just a placeholder
-            }
-            
-            /*
-
-            dotnet run "A2 2D 93 61 7F DC 0D 8E C6 3E A7 74 51 1B 24 B2" 251 465 255 1311 2101864342 8995936589171851885163650660432521853327227178155593274584417851704581358902 "F2 2C 95 FC 6B 98 BE 40 AE AD 9C 07 20 3B B3 9F F8 2F 6D 2D 69 D6 5D 40 0A 75 45 80 45 F2 DE C8 6E C0 FF 33 A4 97 8A AF 4A CD 6E 50 86 AA 3E DF" AfYw7Z6RzU9ZaGUloPhH3QpfA1AXWxnCGAXAwk3f6MoTx
-
-            */
-            string P3_answer = "Edward Snowden"; 
-            return P3_answer;
-
+            byte[] IV = get_bytes_from_string(args[0]);
+            int g_e = int.Parse(args[1]);
+            int g_c = int.Parse(args[2]);
+            int N_e = int.Parse(args[3]);
+            int N_c = int.Parse(args[4]);
+            int x = int.Parse(args[5]);
+            BigInteger gy = BigInteger.Parse(args[6]);
+            byte[] C = get_bytes_from_string(args[7]);
+            string P = args[8];
+            BigInteger N = BigInteger.Subtract(BigInteger.Pow(2, N_e), N_c);
+            byte[] key = BigInteger.ModPow(gy,x, N).ToByteArray();
+            String decryptedC = DecryptStringFromBytes_Aes(C, key, IV);
+            String encryptedP = BitConverter.ToString(EncryptStringToBytes_Aes(P, key, IV));
+            return decryptedC + "," + encryptedP.Replace("-", " ");
         }
         static void Main(string[] args)
         {
             // args is the array that contains the command line inputs
-            P3(args); // This will run your project code. The autograder will grade the return value of the P1_2 function
+            P3(args);
         }
     }
 }
